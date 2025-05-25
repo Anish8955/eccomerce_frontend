2c897c7417b3f91ef5b917c867ea3d0b11f4e099
Anish khan <anishkhanpro2@gmail.com>
2025-05-25 16:06:46 +0530

    shop&product

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Layout from "./common/Layout";
 import { Rating } from "react-simple-star-rating";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,16 +7,45 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductImgOne from "../assets/images/Mens/five.jpg";
 import ProductImgTwo from "../assets/images/Mens/six.jpg";
 import ProductImgThree from "../assets/images/Mens/seven.jpg";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import { apiUrl } from "../Components/common/http";
 
 const Product = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [rating, setRating] = useState(4);
+  const [product, setProduct] = useState([]);
+  const [productImages, setProductImages] = useState([]);
+  const [productSizes, setProductSizes] = useState([]);
+  const params = useParams();
+
+  const fetchProduct = () => {
+    fetch(`${apiUrl}/get-product/${params.id}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setProduct(result.data);
+          setProductImages(result.data.product_images);
+          setProductSizes(result.data.product_sizes);
+        } else {
+          console.log("something went wrong");
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
   return (
     <Layout>
@@ -33,7 +62,7 @@ const Product = () => {
                   <Link to="/shop">Shop</Link>
                 </li>
                 <li className="breadcrumb-item active" aria-current="page">
-                  Dummy Product Title
+                  {product.title}
                 </li>
               </ol>
             </nav>
@@ -58,36 +87,21 @@ const Product = () => {
                   modules={[FreeMode, Navigation, Thumbs]}
                   className="mySwiper mt-2"
                 >
-                  <SwiperSlide>
-                    <div className="content">
-                      <img
-                        src={ProductImgOne}
-                        alt=""
-                        height={100}
-                        className="w-100"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="content">
-                      <img
-                        src={ProductImgTwo}
-                        alt=""
-                        height={100}
-                        className="w-100"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="content">
-                      <img
-                        src={ProductImgThree}
-                        alt=""
-                        height={100}
-                        className="w-100"
-                      />
-                    </div>
-                  </SwiperSlide>
+                  {productImages &&
+                    productImages.map((product_image) => {
+                      return (
+                        <SwiperSlide>
+                          <div className="content">
+                            <img
+                              src={product_image.image_url}
+                              alt=""
+                              height={100}
+                              className="w-100"
+                            />
+                          </div>
+                        </SwiperSlide>
+                      );
+                    })}
                 </Swiper>
               </div>
               <div className="col-10">
@@ -103,48 +117,51 @@ const Product = () => {
                   modules={[FreeMode, Navigation, Thumbs]}
                   className="mySwiper2"
                 >
-                  <SwiperSlide>
-                    <div className="content">
-                      <img src={ProductImgOne} alt="" className="w-100" />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="content">
-                      <img src={ProductImgTwo} alt="" className="w-100" />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="content">
-                      <img src={ProductImgThree} alt="" className="w-100" />
-                    </div>
-                  </SwiperSlide>
+                  {productImages &&
+                    productImages.map((product_image) => {
+                      return (
+                        <SwiperSlide>
+                          <div className="content">
+                            <img
+                              src={product_image.image_url}
+                              alt=""
+                              className="w-100"
+                            />
+                          </div>
+                        </SwiperSlide>
+                      );
+                    })}
                 </Swiper>
               </div>
             </div>
           </div>
           <div className="col-md-7">
-            <h2>Dummy Product Title</h2>
+            <h2>{product.title}</h2>
             <div className="d-flex">
               <Rating size={20} readonly initialValue={rating} />
               <span className="pt-1 ps-2">10 Reviews</span>
             </div>
             <div className="price h3 py-3">
-              $20 <span className="text-decoration-line-through">$18</span>
+              ₹{parseFloat(product.price)} &nbsp;
+              {product.compare_price && (
+                <span className="text-decoration-line-through">
+                  {parseFloat(product.compare_price)}
+                </span>
+              )}
             </div>
-            <div>
-              100% Original Product <br />
-              Pay on delivery might be available
-              <br />
-              Easy 15 days returns and exchanges
-            </div>
+            <div>{product.short_description}</div>
             <div className="pt-3">
               <strong className="">Select Size</strong>
 
               <div className="sizes pt-2">
-                <button className="btn btn-size ">S</button>
-                <button className="btn btn-size ms-1">M</button>
-                <button className="btn btn-size ms-1">L</button>
-                <button className="btn btn-size ms-1">XL</button>
+                {productSizes &&
+                  productSizes.map((product_size) => {
+                    return (
+                      <button className="btn btn-size ms-2">
+                        {product_size.size.name}
+                      </button>
+                    );
+                  })}
               </div>
             </div>
             <div className="add-to-cart my-3">
@@ -157,21 +174,27 @@ const Product = () => {
 
             <div>
               <strong>SKU: </strong>
-              AA76878
+              {product.sku}
             </div>
           </div>
         </div>
         <div className="row pb-5">
           <div className="col-md-12">
             <Tabs
-              defaultActiveKey="profile"
+              defaultActiveKey="description"
               id="uncontrolled-tab-example"
               className="mb-3"
             >
-              <Tab eventKey="home" title="Description">
-                Tab content for Description
+              <Tab eventKey="description" title="Description">
+                {product.description ? (
+                  <div
+                    dangerouslySetInnerHTML={{ __html: product.description }}
+                  ></div>
+                ) : (
+                  <p>No description available.</p>
+                )}
               </Tab>
-              <Tab eventKey="profile" title="Reviews (10)">
+              <Tab eventKey="reviews" title="Reviews (10)">
                 Tab content for Reviews
               </Tab>
             </Tabs>
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,10 +1,130 @@
-import React from "react";
 import Layout from "./common/Layout";
 import ProductImg from "../assets/images/eight.jpg";
-import { Link } from "react-router-dom";
-
+import { Link, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { apiUrl } from "../Components/common/http";
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();  
+  const [catChecked, setCatChecked] = useState(() => {
+      const category = searchParams.get('category');
+      return category ? category.split(',') : [];
+  });
+  const [brandChecked, setBrandChecked] = useState(() => {
+      const brand = searchParams.get('brand');
+      return brand ? brand.split(',') : [];
+  });
+  
+
+
+   const fetchProducts = () => {
+
+    let search = []
+    let params = "";
+
+    if(catChecked.length > 0){
+      search.push(['category',catChecked])
+    }
+
+    if(brandChecked.length > 0){
+      search.push(['brand',brandChecked])
+    }
+
+
+    if(search.length > 0){
+       params = new URLSearchParams(search)
+       setSearchParams(params)
+    }else{
+       setSearchParams([])
+    }
+
+     
+
+    fetch(`${apiUrl}/get-products?${params}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setProducts(result.data);
+        } else {
+          console.log("something went wrong");
+        }
+      });
+  };
+
+  const fetchCategories = () => {
+    fetch(`${apiUrl}/get-categories`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setCategories(result.data);
+        } else {
+          console.log("something went wrong");
+        }
+      });
+  };
+
+  const fetchBrands = () => {
+    fetch(`${apiUrl}/get-brands`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setBrands(result.data);
+        } else {
+          console.log("something went wrong");
+        }
+      });
+  };
+
+  const handleCategory = (e) =>{
+
+      const {checked,value} = e.target;
+      if(checked){
+        setCatChecked(pre => [...pre, value])
+      }else{
+         setCatChecked(catChecked.filter(id => id != value))
+      }
+      
+  }
+
+   const handleBrand = (e) =>{
+
+      const {checked,value} = e.target;
+      if(checked){
+        setBrandChecked(pre => [...pre, value])
+      }else{
+         setBrandChecked(brandChecked.filter(id => id != value))
+      }
+      
+  }
+
+  useEffect(() => {
+    fetchCategories();
+    fetchBrands();
+    fetchProducts();
+
+  }, [catChecked,brandChecked]);
+
   return (
     <>
       <Layout>
@@ -12,7 +132,7 @@ const Shop = () => {
           <nav aria-label="breadcrumb" className="py-4">
             <ol className="breadcrumb">
               <li className="breadcrumb-item">
-                <a href="/">Home</a>  
+                <a href="/">Home</a>
               </li>
               <li className="breadcrumb-item active" aria-current="page">
                 Shop
@@ -22,142 +142,82 @@ const Shop = () => {
           <div className="row">
             <div className="col-md-3">
               <div className="card shadow border-0">
-                 <div className="card-body p-4">
-                     <h3>Categories</h3>
-                     <ul>
-                      <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Kids</label>
-                      </li>
-                       <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Mens</label>
-                      </li>
-                       <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Women</label>
-                      </li>
-                     </ul>
-                 </div>
+                <div className="card-body p-4">
+                  <ul>
+                    <h3>Categories</h3>
+                    {categories &&
+                      categories.map((category) => {
+                        return (
+                          <li key={`cat-${category.id}`} className="mt-2 mb-1">
+
+                            <input
+                            checked={searchParams.get('category') 
+                              ? searchParams.get('category').includes(category.id):false }
+                           type="checkbox"
+                            value={category.id} 
+                            onChange={handleCategory}
+                            />
+                            <label className="ps-2" htmlFor="">
+                              {category.name}
+                            </label>
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </div>
               </div>
 
-               <div className="card shadow border-0 mt-5 mb-5">
-                 <div className="card-body p-4">
-                     <h3>Brands</h3>
-                     <ul>
-                      <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Mama Earth</label>
-                      </li>
-                       <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Puma</label>
-                      </li>
-                       <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Nike</label>
-                      </li>
-                      <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Levis</label>
-                      </li>
-                      <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Red Tap</label>
-                      </li>
-                      <li className="mt-2">
-                        <input type="checkbox" />
-                        <label className="ps-2" htmlFor="">Killer</label>
-                      </li>
-                     </ul>
-                 </div>
+              <div className="card shadow border-0 mt-5 mb-5">
+                <div className="card-body p-4">
+                  <h3>Brands</h3>
+                  <ul>
+                    {brands &&
+                      brands.map((brand) => {
+                        return (
+                          <li key={`brand-${brand.id}`} className="mb-2">
+                            <input
+                             checked={searchParams.get('brand') 
+                              ? searchParams.get('brand').includes(brand.id):false }
+                            type="checkbox"
+                            value={brand.id}  
+                            onChange={handleBrand}
+                            />
+                            <label className="ps-2" htmlFor="">
+                              {brand.name}
+                            </label>
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </div>
               </div>
-
             </div>
             <div className="col-md-9">
               <div className="row pb-5">
-                   <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                        <Link to="/product">
-                                          <img src={ProductImg} className="w-100" />
-                                        </Link>
-                                          
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <Link to="/product">Red Check Shirt For Men</Link>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
-                                 <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                            <img src={ProductImg} className="w-100" />
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <a href="">Red Check Shirt For Men</a>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
-                                 <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                            <img src={ProductImg} className="w-100" />
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <a href="">Red Check Shirt For Men</a>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
-                                 
-                                 <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                            <img src={ProductImg} className="w-100" />
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <a href="">Red Check Shirt For Men</a>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
-                                <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                            <img src={ProductImg} className="w-100" />
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <a href="">Red Check Shirt For Men</a>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
-                                <div className="col-md-3 col-6">
-                                   <div className="product card border-0">
-                                       <div className="card-img">
-                                            <img src={ProductImg} className="w-100" />
-                                       </div>
-                                       <div className="card-body pt-3">
-                                             <a href="">Red Check Shirt For Men</a>
-                                             <div className="price">
-                                                 $50 <span className="text-decoration-line-through">$80</span>
-                                             </div>
-                                       </div>
-                                   </div>
-                                </div>
+                 {products &&
+                      products.map((product) => {
+                        return (
+                <div className="col-md-3 col-6" key={`product-${product.id}`}>
+                  <div className="product card border-0">
+                    <div className="card-img">
+                      <Link to={`/product/${product.id}`}>
+                        <img src={product.image_url} className="w-100" />
+                      </Link>
+                    </div>
+                    <div className="card-body pt-3">
+                      <Link to={`/product/${product.id}`}>{product.title}</Link>
+                      <div className="price">
+                         {product.price} &nbsp;
+                              {
+                                product.compare_price && <span className="text-decoration-line-through">{product.compare_price}</span>
+                              }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                );
+                      })}
+                
               </div>
             </div>
           </div>
